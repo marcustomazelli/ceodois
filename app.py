847e22578f4328d40eb5118afd22e6391a69e4de
Marcus Tomazelli <tomazelli@proton.me>
2025-01-09 10:53:45 -0400

    fix bug deploy

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def index():
 
 # python web server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", default=5000)))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
